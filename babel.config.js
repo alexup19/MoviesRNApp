e3de730fff744ee0874609d804ded473fe1ae1f6
Alexis Ubri Penzo <alexup@mctekk.com>
2021-06-07 19:28:48 -0400

    feat: Adds module resolver and its babel config

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,30 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: ["babel-plugin-styled-components"],
+// @ts-nocheck
+module.exports = function(api) {
+  const plugins = [
+    ['babel-plugin-styled-components', { ssr: false }],
+    [
+      'module-resolver',
+      {
+        cwd: 'babelrc',
+        root: ['./src'],
+        extensions: ['.js', '.ios.js', '.android.js', '.ts', '.tsx'],
+        alias: {
+          atoms: './src/components/atoms',
+          molecules: './src/components/molecules',
+          organisms: './src/components/organisms',
+          templates: './src/components/templates',
+          utils: './src/utils',
+        },
+      },
+    ],
+  ];
+
+  if (api) {
+    api.cache(true)
+  }
+
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins,
+  };
 };
