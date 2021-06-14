99857578bce21f0b598256cdf6552cb9d034c213
Alexis Ubri Penzo <alexup@mctekk.com>
2021-06-14 19:06:07 -0400

    feat: Ads .env example and axios interceptor to the projects

---
+++ src/core/axios.ts
@@ -0,0 +1,22 @@
+/* eslint-disable no-param-reassign */
+import axios, { AxiosRequestConfig } from 'axios';
+import Config from 'react-native-config';
+
+const instance = axios.create({
+  baseURL: 'https://api.themoviedb.org',
+});
+
+instance.interceptors.request.use(
+  async (config: AxiosRequestConfig) => {
+    config.params = { ...config.params, api_key: Config.THEMOVIEDB_API_KEY }
+    return config;
+  },
+  (error: any) => Promise.reject(error),
+);
+
+instance.interceptors.response.use(
+  (response: any) => response,
+  async (error: { response: { status: number; }; }) => Promise.reject(error),
+);
+
+export default instance;
