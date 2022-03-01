1b35bbbb1172e5e4b7ebbbd88d0f068b652c861c
Alexis Ubri Penzo <alexup@mctekk.com>
2022-03-01 10:56:01 -0400

    feat: Upgrades RN to 0.67.3

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+};
